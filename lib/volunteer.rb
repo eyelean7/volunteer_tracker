45c2214a8f53afc315fd83a8ecf62a74862e1981
Ilene Gorski <ilene.gorski@gmail.com>
2017-05-05 13:05:40 -0700

    add reamining specs for Volunteer class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -12,4 +12,37 @@ class Volunteer
     first_name.+" ".+last_name
   end
 
+  def save
+    result = DB.exec("INSERT INTO volunteers (first_name, last_name) VALUES ('#{@first_name}', '#{@last_name}') RETURNING id;")
+    @id = result.first()['id'].to_i()
+  end
+
+  def ==(another_volunteer)
+    self.name().==(another_volunteer.name()).&(self.id().==(another_volunteer.id()))
+  end
+
+  class << self
+    def all
+      all_volunteers = DB.exec('SELECT * FROM volunteers ORDER BY last_name;')
+      saved_volunteers = []
+      all_volunteers.each() do |volunteer|
+        id = volunteer['id'].to_i()
+        first_name = volunteer['first_name']
+        last_name = volunteer['last_name']
+        each_volunteer = Volunteer.new({:first_name => first_name, :last_name => last_name, :id => id})
+        saved_volunteers.push(each_volunteer)
+      end
+      return saved_volunteers
+    end
+
+    def find(id)
+      found_volunteer = nil
+      Volunteer.all().each() do |volunteer|
+        if volunteer.id() == id
+          found_volunteer = volunteer
+        end
+      end
+      return found_volunteer
+    end
+  end
 end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -1,11 +1,49 @@
 require('spec_helper')
 
 describe(Volunteer) do
+  volunteer = Volunteer.new({:first_name => "Ilene", :last_name => "Gorski"})
+
+  describe('#id') do
+    it "returns the volunteer id" do
+      expect(volunteer.id()).to eq(volunteer.id())
+    end
+  end
 
   describe('#name') do
     it ("returns the name of the volunteer") do
-      volunteer = Volunteer.new({:first_name => "Ilene", :last_name => "Gorski"})
       expect(volunteer.name()).to eq("Ilene Gorski")
+    end
+  end
+
+  describe('#save') do
+    it ("saves a volunteer to the database") do
+      volunteer.save()
+      expect(volunteer.name()).to eq("Ilene Gorski")
+      # name = Volunteer.find(volunteer.id()).name()
+      # expect(name).to eq("Ilene Gorski")
+    end
+  end
+  
+  describe('#==') do
+    it("returns true if volunteers have the same name and id") do
+      volunteer.save()
+      volunteer2 = Volunteer.find(volunteer.id())
+      expect(volunteer).to eq(volunteer2)
+    end
+  end
+
+  describe('.all') do
+    it "returns all volunteers" do
+      volunteer.save()
+      expect(Volunteer.all()).to eq([volunteer])
+    end
+  end
+
+  describe('.find') do
+    it ("finds a volunteer from the database") do
+      volunteer.save()
+      found = Volunteer.find(volunteer.id())
+      expect(found).to eq(volunteer)
     end
   end
 
